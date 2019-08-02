2e70d7eae02c7a12f7735c8a7d59dd6d87204c45
vinaydoddapaneni <vinaydoddapaneni@gmail.com>
2019-08-02 12:37:19 +0530

    deploy works?

---
+++ precache-manifest.3842aaab72a0a618a641ea9ccd2c674b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83891d9127e76621a14deefd1be8f793",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "8fecc25c471abed1e5f2",
+    "url": "/ecommerce/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "dff8f5e6896d4e47fb65",
+    "url": "/ecommerce/static/js/2.d16ba94f.chunk.js"
+  },
+  {
+    "revision": "8fecc25c471abed1e5f2",
+    "url": "/ecommerce/static/js/main.e548a54e.chunk.js"
+  },
+  {
+    "revision": "e176d1401fcedba87ff8",
+    "url": "/ecommerce/static/js/runtime~main.8409c7fe.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ecommerce/static/media/logo.5d5d9eef.svg"
+  }
+]);
